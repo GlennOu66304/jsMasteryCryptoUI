9bc0f932e55d6b0a4bf56b8c68862ac7e438b76c
Glenn <glenou@yahoo.com>
2022-08-25 23:30:20 +0800

    UI build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { useEffect } from 'react'
 // contract
 import contract from './contracts/NFTCollectible.json'
+import './App.css'
 // contract address
 const contractAddress = "0xc5d404183cb9De6a14eccb849f44A862a43c2C05"
 // abi
 const abt = contract.abi
 function App() {
+  const checkWalletIsConnected = () => { }
+
+  const connectWalletHandler = () => { }
+
+  const mintNftHandler = () => { }
+
+  const connectWalletButton = () => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+        Mint NFT
+      </button>
+    )
+  }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
 
   return (
-    <h1>Hello world</h1>
+    <div className='main-app'>
+      <h1>Scrappy Squirrels Tutorial</h1>
+      <div>
+        {connectWalletButton()}
+      </div>
+    </div>
   )
 }
 
