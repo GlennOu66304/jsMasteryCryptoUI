967f1cdcd407187f30454e90772f2dccfce045d4
Glenn <glenou@yahoo.com>
2022-08-25 23:24:58 +0800

    abi build

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+// contract
+import contract from './contracts/NFTCollectible.json'
+// contract address
+const contractAddress = "0xc5d404183cb9De6a14eccb849f44A862a43c2C05"
+// abi
+const abt = contract.abi
 function App() {
 
   return (
